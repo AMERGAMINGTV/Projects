f309333f1c220a99b0efcb4ad18adbf80f5c6b16
AMERGAMINGTV <74202011+AMERGAMINGTV@users.noreply.github.com>
2020-11-09 17:42:00 +0100

    Update bot.py

--- PythonViewBot/bot.py
+++ PythonViewBot/bot.py
@@ -5,7 +5,7 @@ from selenium import webdriver;
 Timer = 120
 
 #youtube link
-link = 'https://www.youtube.com/watch?v=hW_WFUs3hfQ'
+link = 'https://youtu.be/J1o9XQ9aGVM'
 
 #number of views
 views = 20
